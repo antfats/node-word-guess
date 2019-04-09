8e5b655c505c87a742b2fb500f7bcbef2610bc3c
bob <antfats@hotmail.com>
2019-04-09 14:52:43 -0700

    Done with letter.js? Started word.js

--- Letter.js
+++ Letter.js
@@ -1,22 +1,24 @@
 function LetterGuess(value) {
     this.guessed = false;
     this.letter = value;
-    function replace() {
-        for (var i = 1; i < word.length; i++) {
-            console.log("_");
+        this.toString = function(){
+            if (this.letter === " ") {
+                this.guessed = true;
+                return " ";
+            }
+            else{
+                if(this.guessed === false){
+                    return "_";
+                }
+                else{
+                    return this.letter;
+                }
+            }
         }
-    }
-    function correct() {
-        if (userInput === str) {
-            this.bool = true;
-            console.log(this.str);
+        this.guess = function(guess) {
+            if(guess === this.letter) {
+                this.guessed = true;
+            }
         }
-    }
-
-    function incorrect() {
-        this.bool = true;
-        this.str = charAt(str);
-    }
-
 }
-new LetterGuess(false,"What in the dang nabbit",replace());
+module.exports = Letter;
